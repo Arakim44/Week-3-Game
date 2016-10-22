475730ffd24f9f9b4669ede226aa961c551075fe
Ara Kim <ahrakim44@gmail.com>
2016-10-22 09:58:03 -0500

    not working yet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,12 +3,19 @@
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 var words = {
-      movie: ['Back to the Future','The Godfather','Taxidriver','Interstellar','How to Train Your dragon','The Deer Hunter','Mononoke Hime','When Harry Met Sally','The Lord of the Ring','Harry Potter'],
+      movie: ['Back to the Future','The Godfather','Taxidriver','Interstellar','How to Train Your dragon','The Deer Hunter','Mononoke Hime','When Harry Met Sally','The Lord of the Ring','Harry Potter','Star Wars',],
       dog:  ['Dachshund','Pembroke Welsh Corgi','German Shaperd','Golden Ritriver','Jindo','Shiba Inu  ','Samoyed','Siberian Husky','Alaskan Malamute'],
-      superHero: ['Captain America','Thor','Iron Man','Super Man','Spider Man'],
+      superHero: ['Captain America','Thor','Iron Man','Super Man','Spider Man', 'Luke Cage'],
       videoGame: ['Portal','Super Smash Brothers','Heros of Might and Magic','Ori and The Blind Forest','Five Nights at Freddys'],
-      harryPotter: [''],
+      harryPotter: ['Animagus','Blood Traitor','Muggle','Occulmens','Parselmouth','Order of the Phoenix','Lord Voldmort','Hogwarts','Gringotts','Honeydukes','Felix Felicis','Polyjuice','Acid Pop','Butterbeer','Horcuxes','Pensive','Quidditch','Blast-Ended Skrewt','Hippogriff','Hungarian Horntail','Accio','Avada Kedavra','Crucio','Expecto Patronum','Sectumsemptra','Lumos'],
     };
+
+var word;
+var rand;
+var wordHolder;
+var correct;
+var guesses = [];
+var lives;
 
 
 //creating alphabet ul
@@ -21,18 +28,13 @@ var words = {
     var list = document.createElement('li');
     list.id ='letter';
     list.innerHTML = alphabet[i];
-    // check();
+    check();
     myButtons.appendChild(letters);
     letters.appendChild(list);
   }
 
 
 //when categry is clicked, then it should show the randome word from the bank.
-var word;
-var rand;
-var wordHolder;
-var correct;
-
 function movies(){
   rand = Math.floor(Math.random()*words.movie.length);
   word = words.movie[rand];
@@ -40,65 +42,9 @@ function movies(){
   document.getElementById('category').style.display="none";
   console.log(word);
   console.log(word.length);
-
-  wordHolder = document.getElementById('hold');
-  correct = document.createElement('ul');
-
- //  for (var i = 0; i < word.length; i++){
- //    correct.setAttribute('id','chosen-word');
- //    var guess = document.createElement('li');
- //    guess.setAttribute('class','guess');
- //    if (word[i]===" "){
- //    guess.innerHTML = " ";{
- //    guess.innerHTML = " ";
- //    space = 1;
- //     }else{
- //    guess.innerHTML = "_";
- //  }
- //  guesses.push(guess);
- //  wordHolder.appendChild(correct);
- //   correct.appendChild(guess);
- //  }
- // }
-}
-
-
-
-////
-// function document.getElementById('m').onclick(){
-//
-//   var wordHolder = document.getElementById('hold');
-//  var correct = document.createElement('ul');
-//
-//  for (var i = 0; i < word.length; i++) {
-//    correct.setAttribute('id', 'my-word');
-//    var  guess = document.createElement('li');
-//    guess.setAttribute('class', 'guess');
-//    if (word[i] === "-") {
-//      guess.innerHTML = "-";
-//      space = 1;
-//    } else {
-//      guess.innerHTML = "_";
-//    }
-//
-//    geusses.push(guess);
-//    wordHolder.appendChild(correct);
-//    correct.appendChild(guess);
-//  }
-//
-
-
-////
-
-
-
-
-
-
-
-
-
-
+  word = word.replace(/\s/g, "-");
+  result();
+ }
 
 
 
@@ -108,6 +54,8 @@ function dogs(){
    document.getElementById('categoryNames').innerHTML ="You picked Dogs";
    document.getElementById('category').style.display="none";
    console.log(word);
+   word = word.replace(/\s/g, "-");
+   result();
   }
 
 function superHeros(){
@@ -116,6 +64,8 @@ function superHeros(){
    document.getElementById('categoryNames').innerHTML ="You picked Super Heros";
    document.getElementById('category').style.display="none";
    console.log(word);
+   word = word.replace(/\s/g, "-");
+   result();
 
 }
 
@@ -125,14 +75,47 @@ function videoGames(){
   document.getElementById('categoryNames').innerHTML ="You picked Video Games";
   document.getElementById('category').style.display="none";
   console.log(word);
+  word = word.replace(/\s/g, "-");
+  result();
+}
+
+
+function harryPotters(){
+  rand = Math.floor(Math.random()*words.harryPotter.length);
+  word = words.harryPotter[rand];
+  document.getElementById('categoryNames').innerHTML ="You picked Harry Potter!!";
+  document.getElementById('category').style.display="none";
+  console.log(word);
+  word = word.replace(/\s/g, "-");
+  result();
+}
+
+/////write in jQuery***********
+function result(){
+  wordHolder = document.getElementById('hold');
+  correct = document.createElement('ul');
+  for (var i = 0; i < word.length; i++){
+    correct.setAttribute('id','chosen-word');
+    var guess = document.createElement('li');
+    guess.setAttribute('class','guess');
+    if (word[i]==="-"){
+    guess.innerHTML = " ";
+    space = 1;
+     }else{
+    guess.innerHTML = "_";
+  }
+  guesses.push(guess);
+  wordHolder.appendChild(correct);
+  correct.appendChild(guess);
+  }
 }
 
 
 
 
-
-// check = function () {
-   list.onclick = function () {
+//when clicking the letters, this happens:
+function check() {
+    function list.onclick() {
      var geuss = (this.innerHTML);
      this.setAttribute("class", "active");
      this.onclick = null;
@@ -142,17 +125,29 @@ function videoGames(){
          counter += 1;
        }
      }
-     var j = (word.indexOf(geuss));
-     if (j === -1) {
-       lives -= 1;
-       comments();
-       animate();
+    //  var j = (word.indexOf(geuss));
+    //  if (j === -1) {
+      //  lives -= 1;
+      //  comments();
+      //  animate();
      } else {
-       comments();
+      //  comments();
      }
-  //  }
+ }
  }
 
+///show lives
+//  comments = function () {
+//   showLives.innerHTML = "You have " + lives + " lives";
+//   if (lives < 1) {
+//     showLives.innerHTML = "Game Over";
+//   }
+//   for (var i = 0; i < geusses.length; i++) {
+//     if (counter + space === geusses.length) {
+//       showLives.innerHTML = "You Win!";
+//     }
+//   }
+// }
 
 
 
